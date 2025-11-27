580b19017b82af6303430728c3bd82fbe72f6851
MedNewton <med.newton.2013@gmail.com>
2025-11-27 01:05:48 +0100

    Pricing section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,13 @@ import theme from "@/theme/theme";
 import { Stack, Typography } from "@mui/material";
 
 import Hero from "@/components/hero/hero";
+import Pricing from "@/components/pricing/pricing";
+
 export default function Home() {
   return (
       <Stack component="main" width="100%" minHeight="100vh" sx={{ background: theme.palette.background.default, py: 1.5, px: 2 }}>
         <Hero />
+        <Pricing />
       </Stack>
   );
 }
